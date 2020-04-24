5bb073a58858bd0386e4be375500f6e8c8046826
Evan <johannse@hartwick.edu>
2020-04-24 02:47:49 -0400

    update

--- Driver.py
+++ Driver.py
@@ -22,32 +22,92 @@ while choice != 11:
 
     choice = int(input(">>")) # menu input
 
-
     if choice == 1: # menu choice 1
-        find = my_string
-        if find == re.findall(r'\w[q]\w',my_string):
+        regex = r"\.*[q]\.*" # looking for the letter q
+        if re.search(regex, my_string): # if my_string contains q, print true
             print(True)
-            print("This contains 'q'.")
+            print("This contains the letter 'q'.")
         else:
             print(False)
-            print("This does not contain 'q'.")
+            print("This does not contain the letter 'q'.")
+
+    if choice == 2: # menu choice 2
+        regex = r".*[the].*" # looking for the word 'the'
+        if re.search(regex, my_string):
+            print(True)
+            print("This contains the word 'the'.")
+        else:
+            print(False)
+            print("This does not contain the word 'the'.")
+
+    if choice == 3: # menu choice 3
+        regex = r".*[\*].*" # looking for the metacharacter '*'
+        if re.search(regex, my_string):
+            print(True)
+            print("This contain the metacharacter star '*'.")
+        else:
+            print(False)
+            print("This does not contain the metacharacter star '*'.")
+
+    if choice == 4: # menu choice 4
+        regex = r".*[0-9+].*" # looking for any digits/numbers
+        if re.search(regex, my_string):
+            print(True)
+            print(f"This does contain a digit. The digit(s) was/are {re.search(regex, my_string)}")
+        else:
+            print(False)
+            print("This does not contain a digit.")
+    if choice == 5:
+        regex = r".*[\.+].*"
+        if re.search(regex, my_string):
+            print(True)
+            print("This contains the character period '.' .")
+        else:
+            print(False)
+            print("This does not contain the character period '.' .")
+    if choice == 6:
+        regex = r".*[aeiou]{2}.*"
+        if re.search(regex, my_string):
+            print(True)
+            print(f"This contains repeating vowels. they are {re.search(regex, my_string)}")
+        else:
+            print(False)
+    if choice == 7:
+        regex = r".*[\s+].*"
+        if re.search(regex, my_string):
+            print(True)
+            print("This contains white space.")
+        else:
+            print(False)
+            print("This does not contain white space")
+    if choice == 8:
+        regex = r".*[A-Za-z]{2,3}+.*"
+        if re.search(regex, my_string):
+            print(True)
+            print("This contains repeating letters.")
+        else:
+            print(False)
+            print("This does not contain repeating letters.")
+    if choice == 9:
+        my_string.lower()
+        regex = r".*[hello].*"
+        if re.search(regex, my_string):
+            print(True)
+            print("This contains the word 'Hello'.")
+        else:
+            print(False)
+            print("This does not contain the word 'Hello'.")
+    if choice == 10:
+        regex = r".[@].*\..*"
+        if re.search(regex, my_string):
+            print(True)
+            print("This contains an email address.")
+        else:
+            print("This does not contain an email address.")
 
 
-    elif choice == 2: # menu choice 2
 
-    elif chioce == 3: # menu choice 3
 
-    elif choice == 4: # menu choice 4
 
-    elif choice == 5: # menu choice 5
 
-    elif choice == 6: # menu choice 6
-
-    elif choice == 7: # menu choice 7
-
-    elif choice == 8: # menu choice 8
-
-    elif choice == 9: # menu choice 9
-
-    elif choice == 10: # menu choice 10
 
